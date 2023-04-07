635f9e6fe37b31ff7e25c67a382efd4c7801b3b8
kibroman <you@example.comkibromanx@gmail.com>
2023-04-06 23:13:50 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Characters from './components/Characters.vue'
+</script>
+
+<template>
+  <div>
+    
+  </div>
+  <Characters />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Characters.vue
@@ -0,0 +1,197 @@
+<template>
+  <div>
+    <h3>{{ selectedCharacter }}</h3>
+    <div class="container">
+      <div
+        v-for="character in characters"
+        :key="character.id"
+        class="characters"
+        :style="characterStyle(character)"
+        @mouseover="
+          setActive(character);
+          selection(character);
+        "
+        @mouseleave="active = false"
+      >
+        <img
+          :src="`./portraits/${character.img}`"
+          @click="selection(character)"
+          
+          class="portrait"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      characters: [
+        {
+          id: 1,
+          name: "Ryu",
+          img: "ryu.gif",
+          active: false,
+        },
+        {
+          id: 2,
+          name: "Ken",
+          img: "ken.gif",
+          active: false,
+        },
+        {
+          id: 3,
+          name: "Chun-li",
+          img: "chun-li.gif",
+          active: false,
+        },
+        {
+          id: 4,
+          name: "Cammy",
+          img: "cammy.gif",
+          active: false,
+        },
+        {
+          id: 5,
+          name: "Balrog",
+          img: "balrog.gif",
+          active: false,
+        },
+        {
+          id: 6,
+          name: "Blanka",
+          img: "blanka.gif",
+          active: false,
+        },
+        {
+          id: 7,
+          name: "Deejay",
+          img: "deejay.gif",
+          active: false,
+        },
+        {
+          id: 8,
+          name: "Dhalsim",
+          img: "dhalsim.gif",
+          active: false,
+        },
+        {
+          id: 9,
+          name: "E. Honda",
+          img: "ehonda.gif",
+          active: false,
+        },
+        {
+          id: 10,
+          name: "Feilong",
+          img: "feilong.gif",
+          active: false,
+        },
+        {
+          id: 11,
+          name: "Guile",
+          img: "guile.gif",
+          active: false,
+        },
+        {
+          id: 12,
+          name: "M Bison",
+          img: "mbison.gif",
+          active: false,
+        },
+        {
+          id: 13,
+          name: "Sagat",
+          img: "sagat.gif",
+          active: false,
+        },
+        {
+          id: 14,
+          name: "T Hawk",
+          img: "thawk.gif",
+          active: false,
+        },
+        {
+          id: 15,
+          name: "Vega",
+          img: "vega.gif",
+          active: false,
+        },
+        {
+          id: 16,
+          name: "Zangief",
+          img: "zangief.gif",
+          active: false,
+        },
+      ],
+      active: false,
+      selectedCharacter: null,
+      
+    };
+  },
+  methods: {
+    selection(character) {
+      this.selectedCharacter = `${character.name}`;
+      this.active = true;
+      console.log(`${character.name}`);
+      console.log(this.active);
+    },
+    setActive(character) {
+      this.characters.forEach((c) => {
+        c.active = c === character;
+      });
+    },
+  },
+  computed: {
+    characterStyle() {
+      return (character) => {
+        return {
+          
+          backgroundColor: !character.active ? "#5683d4" : "red",
+        };
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  /* grid-template-rows: repeat(2, 1fr); */
+  gap: 0px 0px;
+  /* grid-template-areas:
+    ". . . . . . . ."
+    ". . . . . . . ."; */
+  line-height: 0;
+  width: 0;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: space-evenly;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin-bottom: 30px;
+  
+  
+}
+
+/* .characters:hover {
+  border: solid thin;
+} */
+
+.characters {
+  background-color:turquoise;
+  border:solid lightgray;
+  border-style: inset;
+
+}
+
+/* .portrait {
+  background-color:aqua;
+} */
+</style>
